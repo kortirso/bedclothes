eaffe9877f6f9a259132204b94a29c0c9a772776
kortirso <kortirso@gmail.com>
2017-09-10 16:03:18 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, '/var/www/html/bedclothes'
 set :deploy_user, 'kortirso'
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/application.yml', 'node_modules/.bin/webpack')
+set :linked_files, fetch(:linked_files, []).push('config/application.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
